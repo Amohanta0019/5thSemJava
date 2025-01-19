4aa6f6d89b9677344816ed587ee1ecdbb8fe5a72
AhanaMohanta <mohantaahana444@gmail.com>
2025-01-19 13:32:06 +0530

    Assignment 07 done

--- Assignment07/Q4.java
+++ Assignment07/Q4.java
@@ -1,6 +1,11 @@
 public class Q4 {
     public static void main(String[] args) {
         String s = "University of Technology";
-        System.out.println(s.indexOf("Tech"));
+        if (s.contains("Tech")) {
+            System.out.println(s.indexOf("Tech"));    
+        }else{
+            System.out.println("string does not contains Tech");
+        }
+        
     }
 }
